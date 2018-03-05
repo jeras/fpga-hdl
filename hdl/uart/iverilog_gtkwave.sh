bc02cf827142e9030fb176f019bb8bd6551f404d
Iztok Jeras <iztok.jeras@gmail.com>
2018-03-05 22:13:52 +0100

    RTL: unfinished recoding of UART

--- hdl/uart/iverilog_gtkwave.sh
+++ hdl/uart/iverilog_gtkwave.sh
@@ -5,7 +5,10 @@ rm uart.out
 rm uart.vcd
 
 # compile the verilog sources (testbench and RTL)
-iverilog -g2012 -o uart.out uart_tb.sv uart.sv
+iverilog -g2012 -o uart.out \
+uart_tb.sv \
+uart_tx.sv \
+uart.sv
 
 # run the simulation
 vvp uart.out
